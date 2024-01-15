0cbd249ae30a6b11de2d39d5f7de73ddfc5ee5e9
furkansimsekli <simseklifurkan0@gmail.com>
2024-01-15 02:56:12 +0100

    project: create validation dataset from cifar10
    cifar10Net: update architecture

--- Project-1/Workspace/helperCIFAR10Data.m
+++ Project-1/Workspace/helperCIFAR10Data.m
@@ -7,6 +7,14 @@
 % ----------
 % Krizhevsky, Alex, and Geoffrey Hinton. "Learning multiple layers of
 % features from tiny images." (2009).
+%
+%
+%
+%
+% 2024.01.15
+% furkansimsekli
+%   Split test dataset to half for validation.
+%
 
 classdef helperCIFAR10Data
     
@@ -28,7 +36,7 @@ classdef helperCIFAR10Data
         
         %------------------------------------------------------------------
         % Return CIFAR-10 Training and Test data.
-        function [XTrain, TTrain, XTest, TTest] = load(dataLocation)         
+        function [XTrain, TTrain, XValidation, TValidation, XTest, TTest] = load(dataLocation)         
             
             location = fullfile(dataLocation, 'cifar-10-batches-mat');
             
@@ -41,8 +49,19 @@ classdef helperCIFAR10Data
             XTrain = cat(4, XTrain1, XTrain2, XTrain3, XTrain4, XTrain5);
             TTrain = [TTrain1; TTrain2; TTrain3; TTrain4; TTrain5];
             
+            % Split the testing data into validation and testing
             [XTest, TTest] = loadBatchAsFourDimensionalArray(location, 'test_batch.mat');
-                      
+            numTestSamples = size(XTest, 4);
+            
+            % Splitting ratio (adjust as needed)
+            validationRatio = 0.5;
+            numValidationSamples = round(validationRatio * numTestSamples);
+            
+            XValidation = XTest(:, :, :, 1:numValidationSamples);
+            TValidation = TTest(1:numValidationSamples);
+            
+            XTest = XTest(:, :, :, numValidationSamples+1:end);
+            TTest = TTest(numValidationSamples+1:end);
         end
     end
 end
--- Project-1/Workspace/stop_sign_detector.m
+++ Project-1/Workspace/stop_sign_detector.m
@@ -4,7 +4,7 @@ url = 'https://www.cs.toronto.edu/~kriz/cifar-10-matlab.tar.gz';
 
 helperCIFAR10Data.download(url,cifar10Data);
 
-[trainingImages,trainingLabels,testImages,testLabels] = helperCIFAR10Data.load(cifar10Data);
+[trainingImages,trainingLabels,validationImages, validationLabels, testImages,testLabels] = helperCIFAR10Data.load(cifar10Data);
 
 size(trainingImages)
 
@@ -15,77 +15,30 @@ categories(trainingLabels)
 [height,width,numChannels, ~] = size(trainingImages);
 
 imageSize = [height width numChannels];
-inputLayer = imageInputLayer(imageSize);
-
-% Convolutional layer parameters
-filterSize = [5 5];
-numFilters = 32;
-
-middleLayers = [
-    
-% The first convolutional layer has a bank of 32 5x5x3 filters. A
-% symmetric padding of 2 pixels is added to ensure that image borders
-% are included in the processing. This is important to avoid
-% information at the borders being washed away too early in the
-% network.
-convolution2dLayer(filterSize,numFilters,'Padding',2)
-
-% Note that the third dimension of the filter can be omitted because it
-% is automatically deduced based on the connectivity of the network. In
-% this case because this layer follows the image layer, the third
-% dimension must be 3 to match the number of channels in the input
-% image.
-
-% Next add the ReLU layer:
-reluLayer()
-
-% Follow it with a max pooling layer that has a 3x3 spatial pooling area
-% and a stride of 2 pixels. This down-samples the data dimensions from
-% 32x32 to 15x15.
-maxPooling2dLayer(3,'Stride',2)
-
-% Repeat the 3 core layers to complete the middle of the network.
-convolution2dLayer(filterSize,numFilters,'Padding',2)
-reluLayer()
-maxPooling2dLayer(3, 'Stride',2)
-
-convolution2dLayer(filterSize,2 * numFilters,'Padding',2)
-reluLayer()
-maxPooling2dLayer(3,'Stride',2)
-
-];
-
-finalLayers = [
-    
-% Add a fully connected layer with 64 output neurons. The output size of
-% this layer will be an array with a length of 64.
-fullyConnectedLayer(64)
-
-% Add an ReLU non-linearity.
-reluLayer
-
-% Add the last fully connected layer. At this point, the network must
-% produce 10 signals that can be used to measure whether the input image
-% belongs to one category or another. This measurement is made using the
-% subsequent loss layers.
-fullyConnectedLayer(numImageCategories)
-
-% Add the softmax loss layer and classification layer. The final layers use
-% the output of the fully connected layer to compute the categorical
-% probability distribution over the image classes. During the training
-% process, all the network weights are tuned to minimize the loss over this
-% categorical distribution.
-softmaxLayer
-classificationLayer
-];
 
 layers = [
-    inputLayer
-    middleLayers
-    finalLayers
-    ];
+    imageInputLayer(imageSize)
+    convolution2dLayer(3, 16, 'Padding', 2)
+    reluLayer()
+    maxPooling2dLayer(2, 'Stride', 2)
+    convolution2dLayer(3, 32, 'Padding', 2)
+    reluLayer()
+    maxPooling2dLayer(2, 'Stride', 2)
+    convolution2dLayer(3, 64, 'Padding', 2)
+    reluLayer()
+    maxPooling2dLayer(2, 'Stride', 2)
+    convolution2dLayer(3, 128, 'Padding', 2)
+    reluLayer()
+    maxPooling2dLayer(2, 'Stride', 2)
+    convolution2dLayer(3, 256, 'Padding', 2)
+    reluLayer()
+    maxPooling2dLayer(2, 'Stride', 2)
+    fullyConnectedLayer(10)
+    softmaxLayer()
+    classificationLayer()
+];
 
-layers(2).Weights = 0.0001 * randn([filterSize numChannels numFilters]);
+% layers(2).Weights = 0.0001 * randn([filterSize numChannels numFilters]);
 
 % Set the network training options
 opts = trainingOptions('sgdm', ...
@@ -95,10 +48,12 @@ opts = trainingOptions('sgdm', ...
     'LearnRateDropFactor', 0.1, ...
     'LearnRateDropPeriod', 8, ...
     'L2Regularization', 0.004, ...
-    'MaxEpochs', 40, ...
+    'MaxEpochs', 20, ...
     'MiniBatchSize', 128, ...
+    'ValidationData', {validationImages, validationLabels}, ...
+    'ValidationPatience', 10, ...
     'Plots', 'training-progress', ...
-    'Verbose', false);
+    'Verbose', true);
 
 cifar10Net = trainNetwork(trainingImages, trainingLabels, layers, opts);
 
@@ -150,7 +105,7 @@ options = trainingOptions('sgdm', ...
     'LearnRateDropPeriod', 100, ...
     'MaxEpochs', 100, ...
     'Plots', 'training-progress', ...
-    'Verbose', false);
+    'Verbose', true);
 
 % Train an R-CNN object detector. This will take several minutes.    
 rcnn = trainRCNNObjectDetector(stopSigns, cifar10Net, options, ...
